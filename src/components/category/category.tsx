fecb32459f753bce92e7cd2c5ee3563ae98e7c44
Iakab Fineas <145750236+Iakab@users.noreply.github.com>
2024-02-22 17:37:12 +0200

    Improved the search bar component and added react-slick for the items… (#7)
    * Improved the search bar component and added react-slick for the items carousel

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -1,25 +1,69 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
-import { RecipeItem, Recipes } from 'utils/api/api.types';
+import { RecipeItem } from 'utils/api/api.types';
 
-import Item from 'components/item/item.component';
+import ItemsSlider from 'components/items-slider/items-slider';
+import PreviewItem from 'components/preview-item/preview-item.component';
+
+import { CategoryType } from 'context/categories.context';
+
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import './category.scss';
 
 type CategoryProps = {
-  category: [string, Recipes];
+  category: CategoryType;
 };
 
 const Category: React.FC<CategoryProps> = ({ category }) => {
+  const [targetRecipe, setTargetRecipe] = useState<RecipeItem>();
+  const { categoryName, recipes } = category;
+
+  const settings = {
+    className: 'slider',
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3.9,
+    slidesToScroll: 5,
+  };
+
+  const handlePreview = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    const { id } = event.currentTarget;
+
+    setTargetRecipe(
+      recipes?.find((item: RecipeItem) => item.id === Number(id)),
+    );
+  };
+
   const item = useMemo(
     () =>
-      category[1]?.map((recipe: RecipeItem) => (
-        <Item key={recipe.id} recipe={recipe} />
+      recipes?.map((recipe: RecipeItem) => (
+        <ItemsSlider
+          handlePreview={handlePreview}
+          key={recipe.id}
+          recipe={recipe}
+        />
       )),
     [category],
   );
 
-  return <div className="category">{item}</div>;
+  return (
+    <div className="category">
+      {targetRecipe && (
+        <PreviewItem recipe={targetRecipe} setTargetRecipe={setTargetRecipe} />
+      )}
+      <div className="title">
+        <h2>{categoryName.toUpperCase()}</h2>
+      </div>
+      <Slider {...settings}>{item}</Slider>
+    </div>
+  );
 };
 
 export default Category;
--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,36 +1,45 @@
 import { useMemo } from 'react';
 
-import { useCategories } from 'context/categories.context';
+import { useCategories, CategoryType } from 'context/categories.context';
 import { useSearchItems } from 'context/search.context';
 
-import Category from 'components/category/category';
 import SearchItems from 'components/search-items/search-items';
+import Category from 'components/category/category';
 
 import Loading from 'react-loading';
 
 import './home.styles.scss';
 
 const Home = () => {
-  const searchItems = useSearchItems();
+  const { searchItems, isLoading } = useSearchItems();
   const categories = useCategories();
 
-  if (!categories) {
+  if (!categories || isLoading) {
     return <Loading type="spin" color="#000" className="loading" />;
   }
 
   const category = useMemo(
     () =>
-      Object.entries(categories).map((item) => (
-        <Category key={item[0]} category={item} />
+      categories.map((item: CategoryType, index: number) => (
+        <Category key={index} category={item} />
       )),
+
     [categories],
   );
 
   return (
     <div className="home">
-      <div className="body">
-        {searchItems ? <SearchItems items={searchItems} /> : category}
+      <div className="menu">
+        <p>Menu</p>
       </div>
+      {searchItems ? (
+        <SearchItems items={searchItems} />
+      ) : (
+        <div className="categories">
+          <h2>DISCOVER NEW RECIPES</h2>
+          {category}
+        </div>
+      )}
     </div>
   );
 };
